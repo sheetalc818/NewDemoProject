a030cd0efa0c198359da83b99cad34bba2e9f463
Pranali0908 <pranali.lembhe77@gmail.com>
2020-02-08 14:54:29 +0530

    Added code for PowerCalculator using Spring Boot

---
+++ src/main/java/com/example/controller/PowerCalculatorController.java
@@ -0,0 +1,27 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dto.NumberDTO;
+import com.example.service.CalculatorService;
+
+@RequestMapping("/calculator")
+@RestController
+public class PowerCalculatorController {
+	@Autowired
+	CalculatorService service;
+
+	@PostMapping("/compute")
+	public int registration(@RequestBody NumberDTO numberDTO) {
+		System.out.println("in controller");
+
+		int response = service.calculate(numberDTO);
+		return response;
+	}
+
+}
